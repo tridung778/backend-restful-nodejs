57b44e566c2c8f43042310714a3a7bb2bd15ecd3
Dương Trí Dũng <crytv778@gmail.com>
2024-08-29 18:47:06 +0700

    #21. Static files

--- src/server.js
+++ src/server.js
@@ -14,6 +14,8 @@ const __dirname = path.dirname(__filename);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   //   res.send("Hello World!");
   res.render("sample");
