9a8b06b3670929a0dc3156646d30e21e8bc84931
Dương Trí Dũng <crytv778@gmail.com>
2024-08-29 18:34:47 +0700

    #20. DevTool - Nodemon

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-
+import { fileURLToPath } from "url";
 dotenv.config();
 
 const app = express();
 const post = process.env.POST || 3000;
 const hostname = process.env.HOST_NAME;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
