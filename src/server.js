1b4f8d47bd6f3eca112df2714e8863502879b169
Dương Trí Dũng <crytv778@gmail.com>
2024-08-29 18:00:39 +0700

    #18. Template (View) Engine

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import expess from "express";
+import path from "path";
+
 const app = expess();
 const post = 8080;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  //   res.send("Hello World!");
+  res.render("sample");
 });
 
 app.listen(post, () => {
