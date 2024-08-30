46bd302a797c9a16acc9d054a1fd4f528525d951
Dương Trí Dũng <crytv778@gmail.com>
2024-08-30 11:23:38 +0700

    #30. Sử Dụng MySQL với Node.JS

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { configViewEngine } from "./config/viewEngine.js";
 import router from "./routes/web.js";
+import mysql from "mysql2";
 
 dotenv.config();
 
@@ -13,6 +14,18 @@ configViewEngine(app);
 
 app.use("/test", router);
 
+//test connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "hoidanit",
+});
+
+// A simple SELECT query
+connection.query("select * from users", function (err, results, fields) {
+  console.log("Result====>", results); // results contains rows returned by server // fields contains extra meta data about results, if available
+});
 app.listen(post, hostname, () => {
   console.log(`Server running at http://${hostname}:${post}`);
 });
