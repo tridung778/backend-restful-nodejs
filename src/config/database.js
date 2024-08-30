f9a038c87c3c4d25799b1441f78c1f8523ec339c
Dương Trí Dũng <crytv778@gmail.com>
2024-08-30 11:44:08 +0700

    #33. Connection Pool Pattern

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,14 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default connection;
